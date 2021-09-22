7fdbcf2dd10e4ff3ad49f71810502f5ee3194085
John J. Watson <outlawstar4761@gmail.com>
2021-09-22 20:26:32 +0100

    setting timezone for tests

--- test/AccessLogParser.js
+++ test/AccessLogParser.js
@@ -1,6 +1,8 @@
 const AccessLogParser = require('../index');
 const expect = require('chai').expect;
 
+process.env.TZ = 'America/Chicago'; 
+
 let logStr = `64.128.132.200 - - [25/Aug/2021:13:01:26 -0500] "OPTIONS /song/search/album/Ravishing%20Grimness HTTP/1.1" 200 424 "https://outlawdesigns.io/" "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0"`;
 
 describe('parseIp()',function(){
